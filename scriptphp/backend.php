5015be8e28bcc1802d89c223fa84cee23e0d8e9f
chokiwild <lou.dvl@eduge.ch>
2017-10-12 11:20:36 +0200

    c

--- scriptphp/backend.php
+++ scriptphp/backend.php
@@ -1,3 +1,9 @@
 <?php
-
+CONST HOST = "http://sterne.lab.ecinf.ch";
+CONST PASSWD = "Super";
+CONST USER = "POCAdmin";
+CONST DBNAME = "POCData";
+function connectDB(){
+    $db = new PDO("mysql:host=".HOST.";dbname=".DBNAME."", USER, PASSWD);
+}
 ?>
