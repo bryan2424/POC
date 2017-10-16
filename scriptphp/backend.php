f7b85361169e9a6fe965afe6299a775e8c52fc5c
chokiwild <lou.dvl@eduge.ch>
2017-10-16 07:56:25 +0200

    m

--- scriptphp/backend.php
+++ scriptphp/backend.php
@@ -1,9 +1,27 @@
 <?php
-CONST HOST = "http://sterne.lab.ecinf.ch";
-CONST PASSWD = "Super";
-CONST USER = "POCAdmin";
-CONST DBNAME = "POCData";
+define('HOST','sterne.lab.ecinf.ch:3307');
+define('PASSWD','Super');
+define('USER','POCAdmin');
+define('DBNAME','POCData');
 function connectDB(){
-    $db = new PDO("mysql:host=".HOST.";dbname=".DBNAME."", USER, PASSWD);
+    try {
+        $db = new PDO("mysql:host=".HOST.";dbname=".DBNAME, USER, PASSWD);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (Exception $e) {
+        die("Erreur : ".$e->getMessage());
+    }
+    return $db;
+}
+
+function getEvents(){
+    try {
+        $connect = connectDB();
+        $req = $connect->prepare("SELECT * FROM T_EVENTS");
+        $req->execute();
+        $result = $req->fetchAll(PDO::FETCH_ASSOC);
+    } catch (Exception $e) {
+        throw $e;
+    }
+    return $result;
 }
 ?>
--- scriptphp/frontend.php
+++ scriptphp/frontend.php
@@ -1,1 +1,13 @@
+<?php
+include("backend.php");
 
+function showEvents(){
+    $events = getEvents();
+    $result = "<table>";
+    foreach($events as $value){
+        $result .= '<tr><td>'.$value["eventName"].'</td><td>'.$value["eventDate"].'</td></tr>';
+    }
+    $result .= "</table>";
+    echo $result;
+}
+?>
