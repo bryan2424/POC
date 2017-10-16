0c3c627cd379c7df3d4275bab8a3f4451afbd81d
chokiwild <lou.dvl@eduge.ch>
2017-10-16 15:38:31 +0200

    m

--- scriptphp/frontend.php
+++ scriptphp/frontend.php
@@ -3,16 +3,16 @@ require_once 'backend.php';
 
 function showEvents($eventsTime){
     $events = getEvents($eventsTime);
-    $result = "<table border='1'><tr><th>Événement</th><th>Date</th><th>Commence à</th><th>Finit à</th></tr>";
+    $result = "<table border='1'><tr><th>Événement</th><th>Date</th><th>Commence à</th><th colspan='1'>Finit à</th></tr>";
     foreach($events as $value){
-        $result .= '<tr><td>'.$value["eventName"].'</td><td>'.$value["eventDate"].'</td><td>'.$value["eventStart"].'</td><td>'.$value["eventEnd"].'</td><td><a href="../scriptphp/scriptDelete.php?idEvent='.$value["idEvent"].'>Annuler</a></td></tr>';
+        $result .= '<tr><td>'.$value["eventName"].'</td><td>'.$value["eventDate"].'</td><td>'.$value["eventStart"].'</td><td>'.$value["eventEnd"].'</td><td><a href="../scriptphp/scriptDelete.php?idEvent='.$value["idEvent"].'">Annuler</a></td></tr>';
     }
     $result .= "</table>";
     echo $result;
 }
 
 function genRecap($eventTitle,$eventDate,$eventStart,$eventEnd){
-    $result = "<form method='POST' action='../scriptphp/scriptConfirmation.php'><input type='hidden' name='eventName' value='".$eventTitle."'><input type='hidden' name='eventDate' value='".$eventDate."'><input type='hidden' name='eventStart' value='".$eventStart."'><input type='hidden' name='eventEnd' value='".$eventEnd."'><table><tr><td>Nom de l'événement :</td><td>".$eventTitle."</td></tr><tr><td>Date prévue :</td><td>".$eventDate."</td></tr><tr><td>Heure de début :</td><td>".$eventStart."</td></tr><tr><td>Heure de fin :</td><td>".$eventEnd."</td></tr><tr><td colspan='3'><input type='submit'><input type=\"button\" onclick=\"window.location.replace('index.php')\" value=\"Annuler\" /> </td></tr></table></form>";
+    $result = "<form method='POST' action='../scriptphp/scriptConfirmation.php'><input type='hidden' name='eventName' value='".$eventTitle."'><input type='hidden' name='eventDate' value='".$eventDate."'><input type='hidden' name='eventStart' value='".$eventStart."'><input type='hidden' name='eventEnd' value='".$eventEnd."'><table><tr><td>Nom de l'événement :</td><td>".$eventTitle."</td></tr><tr><td>Date prévue :</td><td>".$eventDate."</td></tr><tr><td>Heure de début :</td><td>".$eventStart."</td></tr><tr><td>Heure de fin :</td><td>".$eventEnd."</td></tr><tr><td><input type='submit'></td></tr></table></form>";
     return $result;
 }
 ?>
